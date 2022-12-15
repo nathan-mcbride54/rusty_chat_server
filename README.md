43ae23f1ff2e5047ac8be7e2b530c075aaef996a
Nathan McBride <nathan@mcbridewebservices.ca>
2022-12-15 12:52:44 -0500

    Corrected shared state

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # rusty_chat_server
+
+A basic Telnet server implemented in Rust.
+
+Start server with cargo run
+
+Connect client with telnet localhost 8080
+
+Coming features:
+
+	- Numbered chatrooms
+	- Server logging
+	- Client usernames / passwords
+	- Custom client software
+	- Database connection for saving chats
+	- Chat encryption
--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ async fn main() {
     // Create TCP Listener and bind to port 8080. Await connections, and unwrap content.
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let(tx, _rx) = broadcast::channel::<String>(MAX_CLIENTS);
+    let(tx, _rx) = broadcast::channel(MAX_CLIENTS);
 
     loop { // New connection!
 
         // Create a socket that accepts connections on our TCP listener.
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
 
         let tx = tx.clone();
         let mut rx = tx.subscribe();
@@ -25,24 +25,31 @@ async fn main() {
         tokio::spawn(async move {  // Spawn a new thread to run the below code.
 
             // Split the socket into Read / Write components
-            let (sock_read, mut sock_write) = socket.split();
+            let (reader, mut writer) = socket.split();
 
             // Create a new buffer reader, and a line to serve as the string buffer.
-            let mut reader = BufReader::new(sock_read);
+            let mut reader = BufReader::new(reader);
             let mut line = String::new();
 
-            loop { // The R/W loop
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break; // Nothing to read
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+
+                        if result.unwrap() == 0 { break; }
+
+                        tx.send((line.clone(), addr)).unwrap();
+
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
                 }
-
-                tx.send(line.clone()).unwrap();
-
-                let msg = rx.recv().await.unwrap();
-
-                sock_write.write_all(msg.as_bytes()).await.unwrap();
-                line.clear(); // Empty the buffer
             }
         });
     }
